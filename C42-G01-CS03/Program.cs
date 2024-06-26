98f67a60f75eb2ae58722f509fbbf4c9780c3a80
Mahmoud Ahmed <Mahmoud Ahmed@DESKTOP-3CFC79A>
2024-06-26 03:43:06 +0300

    Question 2

--- C42-G01-CS03/Program.cs
+++ C42-G01-CS03/Program.cs
@@ -8,19 +8,35 @@
 
 
             #region Question 1
-            Console.WriteLine("Please Enter Your Number: ");
+            //Console.WriteLine("Please Enter Your Number: ");
 
-            int Number = int.Parse(Console.ReadLine() ?? "0");
+            //int Number = int.Parse(Console.ReadLine() ?? "0");
 
-            if (Number % 3 == 0 && Number % 4 == 0)
-            {
-                Console.WriteLine("Yes");
-            }
-            else
-                Console.WriteLine("No");
+            //if (Number % 3 == 0 && Number % 4 == 0)
+            //{
+            //    Console.WriteLine("Yes");
+            //}
+            //else
+            //    Console.WriteLine("No");
 
             #endregion
 
+
+
+            #region Question 2
+            
+            Console.Write("Please Enter a Number: ");
+
+            int Number = int.Parse(Console.ReadLine() ?? "0");
+
+            if(Number < 0) 
+            {
+                Console.WriteLine("negative");
+            }
+            else
+                Console.WriteLine("positive");
+
+            #endregion
 
 
         }
