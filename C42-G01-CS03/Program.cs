c1ececa62fb9a75884420628b4c581e4ccec0be4
Mahmoud Ahmed <Mahmoud Ahmed@DESKTOP-3CFC79A>
2024-06-26 04:13:56 +0300

    Question 5

--- C42-G01-CS03/Program.cs
+++ C42-G01-CS03/Program.cs
@@ -42,28 +42,79 @@
 
             #region Question 3
 
-            int max = int.MinValue; int min = int.MaxValue;
+            //int max = int.MinValue; int min = int.MaxValue;
 
-            for (int i = 0; i < 3; i++)
-            {
-                Console.Write($"Please Enter the {i + 1}'s Number: ");
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    Console.Write($"Please Enter the {i + 1}'s Number: ");
 
-                int Number = int.Parse(Console.ReadLine() ?? "0");
+            //    int Number = int.Parse(Console.ReadLine() ?? "0");
 
 
 
-                if (Number > max)
-                    max = Number;
+            //    if (Number > max)
+            //        max = Number;
 
-                if (min > Number)
-                    min = Number;
-            }
+            //    if (min > Number)
+            //        min = Number;
+            //}
 
-            Console.WriteLine($"Max Element = {max}");
+            //Console.WriteLine($"Max Element = {max}");
 
-            Console.WriteLine($"Min Element = {min}");
+            //Console.WriteLine($"Min Element = {min}");
 
             #endregion
+
+
+
+            #region Question 4
+
+            //Console.Write("Please Enter a Number: ");
+
+            //int Number = int.Parse(Console.ReadLine() ?? "0");
+
+            //if(Number %2 == 0)
+            //    Console.WriteLine("Number is Even");
+            //else
+            //    Console.WriteLine("Number is Odd");
+
+            #endregion
+
+
+            #region Question 5
+
+            Console.Write("Please Enter a charcter: ");
+
+            char alph = char.Parse(Console.ReadLine() ?? "");
+
+            switch (alph)
+            {
+                case 'a' :
+                    goto case 'u';
+
+                case 'e':
+                    goto case 'u';
+
+                case 'i':
+                    goto case 'u';
+
+                case 'o':
+                    goto case 'u';
+
+                case 'u':
+                    Console.WriteLine("Vowel");
+                    break;
+
+                default:
+                    Console.WriteLine("consonant");
+                    break;
+
+            }
+
+            #endregion
+
+
+
 
         }
     }
